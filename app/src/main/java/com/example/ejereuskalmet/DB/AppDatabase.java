19a6c308f6343ff52d7f83a711e33cb09777d06f
XabiJauregiBravo <ikasg@plaiaundi.net>
2022-01-11 09:50:50 +0100

    SegundoCommit, de prueba

--- app/src/main/java/com/example/ejereuskalmet/DB/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.ejereuskalmet.DB;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Balizas.class,Datos.class}, version = 12)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract BalizasDao balizasDao();
-}
